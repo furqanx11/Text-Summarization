4a14b0cdc74882c84dbed96e7e0fd16adc2af593
Furqan Ahmed <ahmedfurqan510@gmail.com>
2024-04-24 15:10:46 +0500

    Stage 01 Data Ingestion complete

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from TextSummarizer.logging import logger
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is a test log message")
+STAGE_NAME = "Data Ingestion"
+try:
+    logger.info(f"Pipeline stage: {STAGE_NAME} started")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f"Pipeline stage: {STAGE_NAME} completed")
+except Exception as e:
+    logger.error(f"Pipeline stage: {STAGE_NAME} failed")
+    raise e
