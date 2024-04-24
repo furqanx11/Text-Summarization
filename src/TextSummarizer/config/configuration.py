357f775fa2312837c707fc6a5b6204956fa374c5
Furqan Ahmed <ahmedfurqan510@gmail.com>
2024-04-24 22:23:19 +0500

    Stage 02 Data Validation complete

--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -1,12 +1,14 @@
 from TextSummarizer.constants import *
 from TextSummarizer.utils.common import read_yaml, create_directories
-from TextSummarizer.entity import (DataIngestionConfig)
+from TextSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
         self,
-        config_filepath = Path("config/config.yaml"),
-        params_filepath = Path("params.yaml")):
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,):
+        # config_filepath = Path("config/config.yaml"),
+        # params_filepath = Path("params.yaml")):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
@@ -28,4 +30,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-        
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+            config = self.config.data_validation
+
+            create_directories([config.root_dir])
+
+            data_validation_config = DataValidationConfig(
+                root_dir = config.root_dir,
+                STATUS_FILE= config.STATUS_FILE,
+                ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES,
+            )
+
+            return data_validation_config
